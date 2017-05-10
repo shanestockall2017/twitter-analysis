8e88cb622bae3f0f704a57c64f04cbf60bcc46dd
Shane Stockall <Shane Stockall>
2017-05-10 15:30:58 -0500

    fuck api limits

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-"# twitter-analysis" 
+# Twitter Analysis Tool
+
+### Analyze any public Twitter profile, with graph visualization!
+
+## Network Analysis
+
+By reading the data in a public Twitter profile, as well as the public profiles in-network, this tool builds a profile of the most influential people in a given network and provides and ordered list of influencers.
+
+## Graph Visualization
+
+This tool will take a list of influencers and visualize them. 
--- analysis.py
+++ analysis.py
@@ -4,7 +4,11 @@
 import re
 import twitter
 import sys
-from textblob import textblob
+import plotly.plotly as plotly
+import networkx as nx
+from plotly.graph_objs import *
+
+#from textblob import textblob
 
 
 ##########################################################
@@ -15,7 +19,15 @@ API_KEY = ""
 SECRET = ""
 ACCESS_TOKEN = ""
 ACCESS_TOKEN_SECRET = ""
-API_INSTANCE = None
+API_INSTANCE = twitter.Api(consumer_key =       API_KEY,
+					  		   consumer_secret =    SECRET,
+					  		   access_token_key =   ACCESS_TOKEN,
+					  		   access_token_secret = ACCESS_TOKEN_SECRET)
+
+USERS = ['@shanestockall']
+
+LANGUAGES = ['en']
+
 
 
 ##########################################################
@@ -26,26 +38,17 @@ class Person:
 	def __init__(self):
 		self.influence = 0
 		self.handle = ""
+		self.avg_favorites = 0
+		self.avg_retweets = 0
 
 
 ##########################################################
 ######################## METHODS #########################
 ##########################################################
 
-# authenticates with twitter API
-def twitter_api():
-	API_INSTANCE = twitter.Api(consumer_key =       API_KEY,
-					  		   consumer_secret =    SECRET,
-					  		   access_token_key =   ACCESS_TOKEN,
-					  		   access_token_secret = ACCESS_TOKEN_SECRET)
-
-	print api.VerifyCredentials()
-
 # gets a list of followers, given a person
 def get_network(person):
-	api = API_INSTANCE
-
-	network = api.GetFriends()
+	network = API_INSTANCE.GetFriends(screen_name=person)
 
 	return network
 
@@ -57,10 +60,22 @@ def get_tweets(person):
 
 	return list_tweets
 
+def get_num_favorites(tweet):
+	if tweet.favorite_count: 
+		return int(tweet.favorite_count)
+	else:
+		return 0
+
+def get_num_retweets(tweet):
+	if tweet.retweet_count:
+		return int(tweet.retweet_count)
+	else:
+		return 0
+
 # analyzes a person's network to determine their influence, given a person
 def get_influence(person):
-	avg_retweets = 1
-	avg_favorites = 1
+	avg_retweets = 0
+	avg_favorites = 0
 	num_favorites = 0
 	num_retweets = 0
 	influence = 0
@@ -81,11 +96,13 @@ def get_influence(person):
 	avg_retweets = float(num_retweets) / num_tweets
 	avg_favorites = float(num_favorites) / num_tweets
 
-	influence = 0.1 * network_size + avg_favorites + avg_favorites
+	#this is really not a good metric of influence.
+	#avg_retweets also includes things like celeb retweets, so inflation is a thing
+	influence = 0.1 * network_size + avg_retweets + avg_favorites
 
-	return influence
+	return influence, avg_retweets, avg_favorites
 
-
+'''
 # looks through a persons followers/following to find the most influential person
 def most_influential_in_network(person):
 	highest_influence = 0
@@ -120,7 +137,9 @@ def most_influential_in_extended_network(person):
 			most_influential_person = person
 
 	return most_influential_person, highest_influence
+'''
 
+#rip api limits
 def ordered_list_of_people_in_network(person):
 	list_of_people = []
 	network = get_network(person)
@@ -128,13 +147,32 @@ def ordered_list_of_people_in_network(person):
 	for person in network:
 		p = Person()
 		p.handle = person
-		p.influence = get_influence(person)
+		p.influence, p.avg_retweets, p.avg_favorites = get_influence(person)
 		list_of_people.append(p)
 
+	list_of_people.sort(key=lambda x: x.influence, reverse=True)
+
+	return list_of_people
+
+'''
 def main():
-	API_INSTANCE = twitter_api()
+	print API_INSTANCE.VerifyCredentials()
+
+	list_of_people = ordered_list_of_people_in_network('@shanestockall')
+
+	f = open("people.txt", "w+")
+
+	for person in list_of_people:
+		write_string = "username: " + str(person.handle) + "influence: " + str(person.influence) + "average RT: " + str(person.avg_retweets) + "average favorites: " + str(person.avg_favorites) + "\n"
+		print write_string
+		f.write(write_string)
+
+	f.close()
 
 
-	ordered_list_of_people_in_network(sys.argv[1])
+if __name__ == "__main__":
+	main()
+
 # gets the sentiment of a tweet, given a particular tweet
 # def get_tweet_sentiment(tweet):
+'''
